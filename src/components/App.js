86fb2577d5bd355c864961022e1dd1ebf19dd752
Santosh Balchandran <santoshbalchandran@gmail.com>
2022-06-03 09:04:46 +0530

    Retain the logged in user after hard reload

--- src/components/App.js
+++ src/components/App.js
@@ -11,40 +11,60 @@ import Loader from './Loader';
 class App extends React.Component {
   state = {
     articles: null,
-    user: null
+    user: null,
   };
   setUser = (user) => {
     console.log(user);
-    this.setState({user});
-  }
+    this.setState({ user });
+  };
   componentDidMount() {
-    console.log(localStorage);
-    this.setState({user: JSON.parse(localStorage.getItem('loggedInUser')) || null});
+    let token = JSON.parse(localStorage.getItem('loggedInUser')).token;
+    fetch('https://mighty-oasis-08080.herokuapp.com/api/user', {
+      method: 'GET',
+      headers: { Authorization: 'Token ' + token },
+    })
+      .then((res) => res.json())
+      .then((data) => this.setState({ user: data.user }));
+
     fetch('https://mighty-oasis-08080.herokuapp.com/api/articles')
       .then((data) => data.json())
       .then(({ articles }) => this.setState({ articles }))
       .catch((error) => console.log(error));
+    window.addEventListener('beforeunload', this.handleUpdateLocalStorage);
   }
   componentWillUnmount() {
-    localStorage.setItem("loggedInUser", JSON.stringify(this.state.user))
+    window.removeEventListener('beforeunload', this.handleUpdateLocalStorage);
   }
-  render() {    
+  handleUpdateLocalStorage = () => {
+    localStorage.setItem('loggedInUser', JSON.stringify(this.state.user));
+  };
+  render() {
     let articles = this.state.articles;
     if (!articles) {
       return <Loader />;
-    }    
+    }
+
     return (
       <div className="container">
         <Router>
-          <Header user= {this.state.user}/>
+          <Header user={this.state.user} />
           <Routes>
-            <Route index element={<Home articles={articles} user ={this.state.user}/>} />
-            <Route path="/home" element={<Home articles={articles} user={this.state.user}/>} />
-            <Route path="/login" element={<Login setUser = {this.setUser}/>} />
+            <Route
+              index
+              element={<Home articles={articles} user={this.state.user} />}
+            />
+            <Route
+              path="/home"
+              element={<Home articles={articles} user={this.state.user} />}
+            />
+            <Route path="/login" element={<Login setUser={this.setUser} />} />
             <Route path="/signup" element={<Signup />} />
-            <Route path="/article/:slug" element={<Article/>} />
-            <Route path="/tag" element={<Tag articles={articles}/>} />
-            <Route path='*' element={<h2>This page cannot be displayed. 404 Error</h2>} />
+            <Route path="/article/:slug" element={<Article />} />
+            <Route path="/tag" element={<Tag articles={articles} />} />
+            <Route
+              path="*"
+              element={<h2>This page cannot be displayed. 404 Error</h2>}
+            />
           </Routes>
         </Router>
       </div>
